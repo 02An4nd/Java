6900e592520e6b44400569d3d870fc251ba2cb53
02An4nd <anandsaxena.0208@gmail.com>
2026-02-06 22:17:53 +0530

    Implement swap using XOR in Q15.java

---
+++ Q15.java
@@ -0,0 +1,11 @@
+class Q15 {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("a=" + a + " b=" + b);
+    }
+}
