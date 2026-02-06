de72b23081a3c5794c0f1cb7dd91c408f24400f6
02An4nd <anandsaxena.0208@gmail.com>
2026-02-06 22:32:35 +0530

    Add Q19.java to check if a number is a power of two

---
+++ Q19.java
@@ -0,0 +1,8 @@
+class Q19 {
+    public static void main(String[] args) {
+        int n = 16;
+
+        boolean result = (n > 0) && ((n & (n - 1)) == 0);
+        System.out.println(result);
+    }
+}
