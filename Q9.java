559660ba1a6e983e4857c81772c02e609ed528d1
02An4nd <anandsaxena.0208@gmail.com>
2026-02-06 21:02:23 +0530

    Add prime number finder in Q9.java
    Implement prime number finder between two user inputs.

---
+++ Q9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        for (int num = num1; num <= num2; num++) {
+
+            if (num <= 1)
+                continue;
+
+            boolean isPrime = true;
+
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
